926354a02c2286f5252c59f391097282836acf2f
kucin666 <96915738+kucin666@users.noreply.github.com>
2022-01-01 09:40:53 +0700

    dirubah dibaris 89, 94, 98

--- bot/helper/mirror_utils/status_utils/aria_download_status.py
+++ bot/helper/mirror_utils/status_utils/aria_download_status.py
@@ -86,14 +86,14 @@ class AriaDownloadStatus(Status):
         LOGGER.info(f"Cancelling Download: {self.name()}")
         download = self.aria_download()
         if download.is_waiting:
-            self.__listener.onDownloadError("Cancelled by user")
+            self.__listener.onDownloadError("Dibatalkan oleh pengguna")
             aria2.remove([download], force=True)
             return
         if len(download.followed_by_ids) != 0:
             downloads = aria2.get_downloads(download.followed_by_ids)
-            self.__listener.onDownloadError('Download stopped by user!')
+            self.__listener.onDownloadError('Unduhan dihentikan oleh pengguna!')
             aria2.remove(downloads, force=True)
             aria2.remove([download], force=True)
             return
-        self.__listener.onDownloadError('Download stopped by user!')
+        self.__listener.onDownloadError('Unduhan dihentikan oleh pengguna!')
         aria2.remove([download], force=True)
